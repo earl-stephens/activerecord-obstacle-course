ff3d37c7ae86aaa76b52ddc9e5910e863360c397
earl-stephens <stephensearl@hotmail.com>
2019-03-19 23:20:38 -0600

    updated test8

--- spec/models/activerecord_obstacle_course_spec.rb
+++ spec/models/activerecord_obstacle_course_spec.rb
@@ -175,7 +175,8 @@ describe 'ActiveRecord Obstacle Course' do
 
     # ------------------ Using ActiveRecord ----------------------
     # Solution goes here
-    orders_less_than_550 = Order.where("amount < 550")
+    # orders_less_than_550 = Order.where("amount < 550")
+    orders_less_than_550 = Order.where("amount < ?", 550)
     # ------------------------------------------------------------
 
     # Expectation
